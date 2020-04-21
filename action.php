c992ae69c4b1d5588067349771c964be154687c3
OLeh <github@ollismail.de>
2020-04-21 07:33:10 +0200

    Update action.php - first changes towards Webex
    First changes towards Webex Teams, removed urlencode (not necessary for Webex Teams?), some renaming.

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * DokuWiki Plugin Rocket.Chat Notifier (Action Component)
+ * DokuWiki Plugin Webex Teams Notifier (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  *
@@ -14,7 +14,7 @@ if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
 if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
-class action_plugin_rocketchatnotifier extends DokuWiki_Action_Plugin {
+class action_plugin_webexteamsnotifier extends DokuWiki_Action_Plugin {
 
   function register(Doku_Event_Handler $controller) {
     $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
@@ -62,13 +62,17 @@ class action_plugin_rocketchatnotifier extends DokuWiki_Action_Plugin {
     );
 
     // attachments
-    if (!empty($SUM)) {
+    /*if (!empty($SUM)) {
       $data['attachments'] = array(array(
         "title_link"       => "{$this->urlize()}",
         "title"            => "Summary",
         "text"             => "{$SUM}\n- {$fullname}",
         "color"            => "#AB4531"
       ));
+    }*/
+    if (!empty($SUM)) {
+      $wturl = ;
+      $data = array("text" => "{$title}\n {$this->urlize()}\n {$SUM}\n- {$fullname}");
     }
 
     // encode data
@@ -98,16 +102,19 @@ class action_plugin_rocketchatnotifier extends DokuWiki_Action_Plugin {
 
     }
 
+    // Header Content-Type: application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
     // submit payload
-    $pay = urlencode($json);
+    //$pay = urlencode($json);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "payload={$pay}");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "{$json}");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
 
     // ideally display only for Admin users and/or in debugging mode
     if ($result === false){
-      echo 'cURL error when posting Wiki save notification to Rocket.Chat+: ' . curl_error($ch);
+      echo 'cURL error when posting Wiki save notification to Webex Teams: ' . curl_error($ch);
     }
 
     // close curl
@@ -155,4 +162,3 @@ class action_plugin_rocketchatnotifier extends DokuWiki_Action_Plugin {
   }
 }
 
-// vim:ts=4:sw=4:et:enc=utf-8:
